e21a0d9faa86a24f48c02c34ce99cd05e202fe1d
kosuke <kosuke.n@tandemdesign.co.jp>
2017-08-24 18:30:35 +0900

    PinBall　課題
    課題のところまで進めましたので、コミットします

---
+++ Assets/BallControllerKadai.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControllerKadai : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+
+
+	//スコアを初期化
+	private int score = 0;
+	//スコアを表示するテキスト
+	private GameObject scoreText;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find ("GameOverText");
+
+
+		//シーン中のScoreTextオブジェクトの取得
+		this.scoreText = GameObject.Find("ScoreText");
+
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+
+		//scoreに現在の点数を表示
+		this.scoreText.GetComponent<Text>().text = score.ToString();
+
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "SmallStarTag"){
+			this.score += 10;
+		}if(col.gameObject.tag == "SmallCloudTag"){
+			this.score += 10;
+		}if(col.gameObject.tag == "LargeStarTag"){
+			this.score += 20;
+		}if(col.gameObject.tag == "LargeCloudTag"){
+			this.score += 20;
+		}
+
+
+
+		Debug.Log (score);
+
+	}
+
+
+
+		
+
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+	//HingiJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	//初期の傾き
+	//Q.これだと一気に動くけど、徐々に動く方法ってあるの？？→調べよう
+	private float defaultAngle = 20;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		//HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		//Q.SetAngleって関数じゃないの？？→解決。後ろにあった。
+		SetAngle(this.defaultAngle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//左矢印キーを押した時左フリッパーを動かす
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		//右矢印キーを押した時右フリッパーを動かす
+		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		//矢印キー離された時フリッパーを元に戻す
+		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+	}
+
+	//フリッパーの傾きを設定
+	//Q.どうして先に設定しないの？？→どこかにあれば良い
+		public void SetAngle(float angle){
+			//Q.jointSprが勝手に変換される。いい方法はない？？→escキー
+			JointSpring jointSpr = this.myHingeJoint.spring;
+			jointSpr.targetPosition = angle;
+			this.myHingeJoint.spring = jointSpr;
+}
+
+}
